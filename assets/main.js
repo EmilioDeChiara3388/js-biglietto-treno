bbce446b827e789753ffab48931401da7c1e34ba
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-09-30 18:37:40 +0200

    fix: changed variables values and removed else

--- assets/main.js
+++ assets/main.js
@@ -9,8 +9,8 @@ L'output del prezzo finale va messo fuori in forma umana (con massimo due decima
 let km = Number(prompt("Inserisci chilometri da percorrere"));
 let age = Number(prompt("Inserisci età del passeggero"));
 let price = km * 0.21;
-let discount;
-let finalPrice;
+let discount = 0;
+let finalPrice = price - discount;
 
 console.log(km);
 console.log(price);
@@ -23,9 +23,6 @@ if (age < 18){
 } else if (age >= 65){
     discount = price - (price / 100 * 60);
     finalPrice = price - discount;
-} else {
-    discount = 0;
-    finalPrice = price;
 }
 console.log("Il prezzo finale del tuo biglietto è " + finalPrice.toFixed(2));
     alert("Il prezzo finale del tuo biglietto è " + finalPrice.toFixed(2));
